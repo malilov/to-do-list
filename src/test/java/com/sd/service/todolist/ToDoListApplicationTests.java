1944ba2ea06f9bce33a1c2b0d3fd89ecee5da8a9
liliana.ortiz <liliana.ortiz@evia.de>
2023-08-05 12:44:11 +0200

    feature project creation

---
+++ src/test/java/com/sd/service/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd.service.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
